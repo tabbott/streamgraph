82635096fc956f9dcd95ce6d01b9e67cd2dcbe42
Travis Abbott <tabbott@genome.wustl.edu>
2015-03-10 14:28:54 -0500

    allow c++11 for executables

--- src/exe/pgraph.cpp
+++ src/exe/pgraph.cpp
@@ -2,35 +2,26 @@
 
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
+
+namespace {
+    typedef std::vector<std::string> VS;
+}
 
 int main(int argc, char** argv) {
-    std::vector<std::string> args;
-    args.push_back("ls");
-    args.push_back("-al");
-
     ProcessGraph pg;
-    int lsid = pg.add(args);
-
-    args.clear();
-    args.push_back("tr");
-    args.push_back("[a-z]");
-    args.push_back("[A-Z]");
-    int trid = pg.add(args);
-
-    args.clear();
-    args.push_back("sed");
-    args.push_back("s/BIN/BEEEEEN/g");
-    int sedid = pg.add(args);
+    int lsid = pg.add(VS{"ls", "-al"});
+    int trid = pg.add(VS{"tr" , "[a-z]", "[A-Z]"});
+    int sedid = pg.add(VS{"sed", "s/BIN/BEEEEEN/g"});
 
     pg.connect(lsid, 1, trid, 0);
     pg.connect(trid, 1, sedid, 0);
 
-    args.clear();
-    args.push_back("cat");
     for (int i = 0; i < 3; ++i) {
         std::stringstream path;
         path << "/tmp/meow" << i << ".txt";
-        int catid = pg.add(args);
+        int catid = pg.add(VS{"cat"});
         pg.process(catid)->fd_map().add_file(1, path.str(), O_CREAT|O_WRONLY, 0644);
         pg.connect(sedid, 1, catid, 0);
     }
